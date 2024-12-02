dd2b6396d5d8dbf395b11edcd492b551c7391871
MinhKhanh614 <167750602+MinhKhanh614@users.noreply.github.com>
2024-12-02 18:11:42 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,22 @@
 /**
- * Giá trị Analog của cảm biến
+ * Tạo 1 biến lưu số đếm, bắt đầu từ số 0
  */
-/**
- * Giá trị (%) của cảm biến
- */
-let dataPercent = 0
-let dataAnalog = 0
-// Bật cổng Serial
-serial.setBaudRate(BaudRate.BaudRate115200)
+let count = 0
 // Xóa toàn bộ nội dung trên LCD (nếu có)
 lcd.clearScreen()
-// Cho hiển thị tiêu đề trước
-lcd.displayText("Light Detector", 1, 1)
-lcd.displayText("[LDR] " + lcd.displaySymbol(lcd.Symbols.sym02), 1, 2)
+// Cho hiển thị nội dung sau trên LCD
+lcd.displayText("     LCD2004 I2C    ", 1, 2)
+lcd.displayText("     MakerLab.vn    ", 1, 3)
+// Dừng 5s để hiển thị, rồi xóa toàn bộ nội dung
+basic.pause(5000)
+lcd.clearScreen()
+// Cho hiển thị tiêu đề "Đếm"
+lcd.displayText("Count:", 1, 1)
 basic.forever(function () {
-    // Đọc giá trị Analog của cảm biến và đổi ra thang (%)
-    dataAnalog = pins.analogReadPin(AnalogPin.P0)
-    dataPercent = Math.round(Math.map(dataAnalog, 0, 1023, 100, 0))
-    // Cho hiển thị giá trị (%) của cảm biến trên LCD
-    lcd.displayText("" + dataPercent + "%  ", 9, 2)
-    // Gửi giá trị (%) của cảm biến lên Serial
-    serial.writeLine("" + (dataPercent))
+    // Tăng giá trị số đếm lên 1 đơn vị
+    count += 1
+    // Hiển thị giá trị đó trên LCD sau tiêu đề "Đếm"
+    lcd.displayText(convertToText(count), 8, 1)
     // Dừng 0.5s
     basic.pause(500)
 })
